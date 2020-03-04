780255707e12a80c7054d5b2b82a8895b75630ef
Rariolu <rcoyneyr82011@gmail.com>
2020-03-04 13:56:03 +0000

    Refactoring and tests

--- Assets/Scripts/NewBehaviourScript1.cs
+++ Assets/Scripts/NewBehaviourScript1.cs
@@ -5,10 +5,12 @@ public class NewBehaviourScript1 : MonoBehaviour
 {
     public string die = "die";
     public int[] rir;
+    public GameObject testObj;
+    public GameObject testObj2;
     // Use this for initialization
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
