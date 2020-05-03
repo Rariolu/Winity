73194a91f634d075805df034c25a373cd8fd24ee
Rariolu <rcoyneyr82011@gmail.com>
2020-05-04 00:01:07 +0100

    Add member class and member struct

--- Assets/Scripts/NewBehaviourScript1.cs
+++ Assets/Scripts/NewBehaviourScript1.cs
@@ -6,16 +6,21 @@ public class NewBehaviourScript1 : MonoBehaviour
     public string die = "die";
     public int[] rir;
     public GameObject testObj;
+    public TempStruct tempStruct;
+    public TempClass tempClass;
     //public GameObject testObj2;
-    // Use this for initialization
-    void Start()
-    {
-        
-    }
+}
 
-    // Update is called once per frame
-    void Update()
-    {
+[System.Serializable]
+public struct TempStruct
+{
+    public int b;
+    public int c;
+}
 
-    }
+[System.Serializable]
+public class TempClass
+{
+    public int c;
+    public int d;
 }
