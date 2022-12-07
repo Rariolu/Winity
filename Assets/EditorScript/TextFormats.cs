c6546d07cdbf21474061c9ce05d1876dea2efe25
Rariolu <rcoyneyr82011@gmail.com>
2022-12-07 23:40:41 +0000

    Added prefab text formats

--- Assets/EditorScript/TextFormats.cs
+++ Assets/EditorScript/TextFormats.cs
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 
 public static class TextFormats
 {
+    #region Scene
+
     #region MainClassFileFormat
-    public const string mainClassFileFormat = 
+    public const string mainSceneClassFileFormat = 
 @"using UnityEngine;
 using System.Collections;
 using WinformsUnity;
@@ -24,7 +26,7 @@ public partial class {0} : BaseScene
     #endregion
 
     #region DesignerClassFileFormat
-    public const string designerClassFileFormat = 
+    public const string designerSceneClassFileFormat = 
 @"using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -57,5 +59,64 @@ partial class {0}
     #endregion
 }}
 ";
+
     #endregion
+
+    #endregion
+
+    #region Prefab
+
+    #region PrefabMainClass
+
+    public const string mainPrefabClassFileFormat =
+@"using UnityEngine;
+using System.Collections;
+using WinformsUnity;
+
+public partial class {0} : Prefab
+{{
+    public {0}() : base(""{1}"")
+    {{
+
+    }} 
+}}
+";
+
+    #endregion
+
+    public const string designerPrefabClassFileFormat =
+@"using UnityEngine;
+using System.Collections;
+using WinformsUnity;
+
+partial class {0}
+{{
+ #region ObjectMapping
+
+    protected override void MapObjects()
+    {{
+{1}
+    }}
+
+    #endregion
+
+    #region GameObjectFunctions
+    protected override void InitialiseGameObjects()
+    {{
+{2}
+    }}
+
+{3}
+    #endregion
+
+    #region Variables
+
+{4}
+    #endregion
+}}
+
+";
+
+    #endregion
+
 }
