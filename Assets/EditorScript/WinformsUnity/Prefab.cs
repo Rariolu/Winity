f6346d790876aa9c7888789b2590c227b2729187
Rariolu <rcoyneyr82011@gmail.com>
2022-12-07 23:43:06 +0000

    Created Prefab class to be a parent to generated custom prefab classes

---
+++ Assets/EditorScript/WinformsUnity/Prefab.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UObject = UnityEngine.Object;
+
+namespace WinformsUnity
+{
+    public abstract class Prefab
+    {
+        public Prefab(string name)
+        {
+            gameObject = new GameObject(name);
+            MapObjects();
+            InitialiseGameObjects();
+        }
+        protected abstract void MapObjects();
+        protected abstract void InitialiseGameObjects();
+        protected GameObject GetGameObject()
+        {
+            return gameObject;
+        }
+        protected UObject GetObject(int id)
+        {
+            return unityObjectMap[id];
+        }
+        protected void SetObject(int id, UObject obj)
+        {
+            if (unityObjectMap.ContainsKey(id))
+            {
+                unityObjectMap[id] = obj;
+            }
+            else
+            {
+                unityObjectMap.Add(id, obj);
+            }
+        }
+
+        GameObject gameObject;
+        Dictionary<int, UObject> unityObjectMap = new Dictionary<int, UObject>();
+    }
+}
